b1c5f59e6cf1d9bfc13c8723751dafb507360774
Yaseer Dauda <143911858+Yaseerdev@users.noreply.github.com>
2025-09-16 23:15:24 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,43 @@
 import '../styles/globals.css';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 import * as gtag from '../lib/analytics';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
-  <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-T8S7BTJPMP"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-T8S7BTJPMP');
-</script>
-
   useEffect(() => {
     const handleRouteChange = (url) => {
       gtag.pageview(url);
     };
-    
+
     router.events.on('routeChangeComplete', handleRouteChange);
-    
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange);
     };
   }, [router.events]);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics Scripts */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-T8S7BTJPMP"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-T8S7BTJPMP', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      {/* Your App */}
+      <Component {...pageProps} />
+    </>
+  );
 }
