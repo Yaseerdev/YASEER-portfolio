99f575ec385a385235848f2498e106eae930fd78
Yaseer Dauda <143911858+Yaseerdev@users.noreply.github.com>
2025-09-16 23:11:28 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,16 @@ import * as gtag from '../lib/analytics';
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-T8S7BTJPMP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-T8S7BTJPMP');
+</script>
+
   useEffect(() => {
     const handleRouteChange = (url) => {
       gtag.pageview(url);
